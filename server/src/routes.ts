00175435c9cf6c5361ece8b1a44cfaa80cd04612
DevSp <contatodevsp@gmail.com>
2020-06-04 16:15:29 -0300

    Rotas

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,48 +1,20 @@
 import express from 'express'
-import path from 'path'
 import knex from './database/connection'
 
-const routes = express.Router()
+import PointsController from './controllers/PointsController'
+import ItemsController from './controllers/ItemsController'
 
-// rota para as imagens dos items
-routes.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+// index, show, create, update, delete
+
+const routes = express.Router()
+const pointsController = new PointsController()
+const itemsController = new ItemsController()
 
 // items 
-routes.get('/items', async (req, res) => {
-    const items = await knex('items').select('*')
-
-    const serializedItems = items.map(item => {
-        const { id, title, image } = item
-        const image_url = `http://localhost:3333/uploads/${image}`
-
-        return { id, title, image_url }
-    })
-
-    return res.json(serializedItems)
-})
+routes.get('/items', itemsController.index)
 
 // points
-routes.post('/points', async (req, res) => {
-    const { name, email, whatsapp, latitude, longitude, city, uf, items } = req.body
-
-    // caso um insert falhe o outro não executa, pois os dois estão vinculados
-    const trx = await knex.transaction()
-
-    const insertedIds = await trx('points').insert({
-        image: 'image-fake',
-        name, email, whatsapp, latitude, longitude, city, uf,
-    })
-
-    const point_id = insertedIds[0]
-
-    const pointItems = items.map((item_id: number) => ({
-        item_id,
-        point_id
-    }))
-
-    await trx('point_items').insert(pointItems)
-
-    return res.json({ success: true })
-})
+routes.post('/points', pointsController.create)
+routes.get('/points/:id', pointsController.show)
 
 export default routes
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express'
+import path from 'path'
 import routes from './routes'
 
 const app = express()
 
 app.use(express.json())
 app.use(routes)
+
+// rota para as imagens dos items
+routes.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.listen(3333, () => console.log('> Rodando na porta: 3333'))
