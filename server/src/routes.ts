5614a7f76d4ceee2142859b333b667234e12b610
DevSp <contatodevsp@gmail.com>
2020-06-04 14:06:43 -0300

    Criando Rota e Cad Point

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import path from 'path'
-import Knex from './database/connection'
+import knex from './database/connection'
 
 const routes = express.Router()
 
@@ -9,14 +9,40 @@ routes.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 // items 
 routes.get('/items', async (req, res) => {
-    const items = await Knex('items').select('*')
+    const items = await knex('items').select('*')
 
-    const serializedItems = items.map(item => ({
-        title: item.title,
-        image_url: `http://localhost:3333/uploads/${item.image}`
-    }))
+    const serializedItems = items.map(item => {
+        const { id, title, image } = item
+        const image_url = `http://localhost:3333/uploads/${image}`
+
+        return { id, title, image_url }
+    })
 
     return res.json(serializedItems)
+})
+
+// points
+routes.post('/points', async (req, res) => {
+    const { name, email, whatsapp, latitude, longitude, city, uf, items } = req.body
+
+    // caso um insert falhe o outro não executa, pois os dois estão vinculados
+    const trx = await knex.transaction()
+
+    const insertedIds = await trx('points').insert({
+        image: 'image-fake',
+        name, email, whatsapp, latitude, longitude, city, uf,
+    })
+
+    const point_id = insertedIds[0]
+
+    const pointItems = items.map((item_id: number) => ({
+        item_id,
+        point_id
+    }))
+
+    await trx('point_items').insert(pointItems)
+
+    return res.json({ success: true })
 })
 
 export default routes
