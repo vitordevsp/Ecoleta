45914c5b65065a4b0596c91385a707a55122a765
DevSp <contatodevsp@gmail.com>
2020-06-04 16:37:08 -0300

    Rota listar pontos de coleta filtrados

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -2,6 +2,24 @@ import { Request, Response } from 'express'
 import knex from '../database/connection'
 
 class PointsController {
+    async index(req: Request, res: Response) {
+        const { city, uf, items } = req.query
+
+        const parsedItems = String(items)
+            .split(',')
+            .map(item => Number(item.trim()))
+
+        const points = await knex('points')
+            .join('point_items', 'points.id', '=', 'point_items.point_id')
+            .whereIn('point_items.item_id', parsedItems)
+            .where('city', String(city))
+            .where('uf', String(uf))
+            .distinct()
+            .select('points.*')
+
+        return res.json(points)
+    }
+
     async show(req: Request, res: Response) {
         const { id } = req.params
 
@@ -23,14 +41,14 @@ class PointsController {
         const { name, email, whatsapp, latitude, longitude, city, uf, items } = req.body
 
         // caso um insert falhe o outro não executa, pois os dois estão vinculados
-        // const trx = await knex.transaction()
+        const trx = await knex.transaction()
 
         const point = {
             image: 'image-fake',
             name, email, whatsapp, latitude, longitude, city, uf,
         }
 
-        const insertedIds = await knex('points').insert(point)
+        const insertedIds = await trx('points').insert(point)
 
         const point_id = insertedIds[0]
 
@@ -39,7 +57,9 @@ class PointsController {
             point_id
         }))
 
-        await knex('point_items').insert(pointItems)
+        await trx('point_items').insert(pointItems)
+
+        trx.commit()
 
         return res.json({ id: point_id, ...point })
     }
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -15,6 +15,7 @@ routes.get('/items', itemsController.index)
 
 // points
 routes.post('/points', pointsController.create)
+routes.get('/points', pointsController.index)
 routes.get('/points/:id', pointsController.show)
 
 export default routes
