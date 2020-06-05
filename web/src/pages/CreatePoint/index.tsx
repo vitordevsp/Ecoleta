89e9f93e70dafc28e34374fc2c7f211ff9d56c31
DevSp <contatodevsp@gmail.com>
2020-06-05 19:16:37 -0300

    Buscando ufs da API do IBGE

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
 import { Map, TileLayer, Marker } from 'react-leaflet'
+import axios from 'axios'
 import api from '../../services/api'
 
 import './styles.css'
@@ -15,13 +16,25 @@ interface Item {
     image_url: string
 }
 
+interface IBGE_UF {
+    sigla: string
+}
+
 const CreatePoint = () => {
 
     const [items, setItems] = useState<Item[]>([])
+    const [ufs, setUfs] = useState<string[]>([])
 
     useEffect(() => {
         api.get('items').then(response => {
             setItems(response.data)
+        })
+    }, [])
+
+    useEffect(() => {
+        axios.get<IBGE_UF[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados').then(response => {
+            const ufInitials = response.data.map(uf => uf.sigla)
+            setUfs(ufInitials)
         })
     }, [])
 
@@ -94,6 +107,9 @@ const CreatePoint = () => {
                             <label htmlFor="uf">Estado (UF)</label>
                             <select name="uf" id="uf">
                                 <option value="0">Selecione uma UF</option>
+                                {ufs.map(uf => (
+                                    <option key={uf} value={uf}>{uf}</option>
+                                ))}
                             </select>
                         </div>
 
